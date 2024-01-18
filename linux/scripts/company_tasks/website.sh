602ddd18e4ad5e94509d9871e93fc53fc1ea3ae9
Diego Cruz <57609866+Kirito-Kun07@users.noreply.github.com>
2024-01-17 23:32:25 -0800

    Create website.sh
    created stuff for the company

---
+++ linux/scripts/company_tasks/website.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#For Debian
+apt install apache2 -y
+systemctl enable apache2
+systemctl start apache2
+ufw allow 'Apache Full'
+echo "<html>
+        <head>
+                <title>apache page</title>
+        </head>
+        <body>
+                <p>hello world</p>
+        </body>
+</html>" > /var/www/html/index.html
+chown www-data:www-data /var/www/html/index.html
+systemctl restart apache2
